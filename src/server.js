6201a1478abb9a9012c1705bf4195aefa14e6e48
Shlok Jain <109564087+shlok30jainmail@users.noreply.github.com>
2025-03-11 10:20:40 +0530

    updated code

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/Database")
 const app = require("./app");
+require('dotenv').config()
 
 // Note always database must connect first then our server must be start
 connectDB().then(()=>{
